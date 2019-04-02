3dac5dd02dee3601bfacca45fc4249c434aa829a
XiaoyingYu <xiy62@pitt.edu>
2019-04-02 15:42:17 -0400

    week5 code

---
+++ week5/web_server/client/build/precache-manifest.88f9535d24c49d728070f4d0efd1283b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9ebcc13a446003855c54117e7f37b87",
+    "url": "/static/media/logo.f9ebcc13.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d43f20abfc19ce0738c",
+    "url": "/static/js/main.3af860e3.chunk.js"
+  },
+  {
+    "revision": "03713c2c0d5d0fcb9e13",
+    "url": "/static/js/2.9a4c80f6.chunk.js"
+  },
+  {
+    "revision": "2d43f20abfc19ce0738c",
+    "url": "/static/css/main.a4239eaf.chunk.css"
+  },
+  {
+    "revision": "03713c2c0d5d0fcb9e13",
+    "url": "/static/css/2.09a7750a.chunk.css"
+  },
+  {
+    "revision": "3c8afe2de8bb5d62c61ae1f455a0a1b7",
+    "url": "/index.html"
+  }
+];
